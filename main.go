4c1e6d4bef91a926e829e1b6715aba027cf94fde
taromn <54003207+taromn@users.noreply.github.com>
2024-03-07 15:52:02 +0000

    add comments

--- main.go
+++ main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/learning-go-book/package_example/formatter"
+	"github.com/learning-go-book/package_example/formatter"  // path to package
 	"github.com/learning-go-book/package_example/math"
 )
 
 func main() {
 	num := math.Double(2)
-	output := print.Format(num)
+	output := print.Format(num)  // print -> package print in ./formatter
 	fmt.Println(output)
 }
